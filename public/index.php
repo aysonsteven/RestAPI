bf747dc7c8665220474a03694851fdd8c307642a
Mark Steven A. Nonato <manonato@icomteq.local>
2017-08-30 20:08:58 +0800

    fix minor bugs

--- public/index.php
+++ public/index.php
@@ -11,23 +11,8 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    $http_origin = $_SERVER['HTTP_ORIGIN'];
-    $default_origin = 'http://localhost:8081';
-    if ($http_origin == "http://localhost:8081" || $http_origin == "http://localhost:8080")
-    {
-//        header("Access-Control-Allow-Origin: $http_origin");
-        $default_origin = $http_origin;
-    }
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', $default_origin )
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
 
-
-$app->post('/hello/{name}', function (Request $request, Response $response) {
+$app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
 
