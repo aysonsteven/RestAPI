f7bc30baa20ae2c4ea1b89a855e449db2789ef92
aysonsteven <aysonsteven@gmail.com>
2017-09-04 01:02:24 +0800

    added generic functions and prepared statements

--- public/index.php
+++ public/index.php
@@ -3,27 +3,28 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 use Slim\App;
+use \functions\functions;
 require '../vendor/autoload.php';
 require '../src/config/db.php';
 
 $app = new App();
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
+//$app->options('/{routes:.+}', function ($request, $response, $args) {
+//    return $response;
+//});
 
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
         ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'POST, DELETE, OPTIONS');
 });
 
-
-$app->post('/hello', function (Request $request, Response $response) {
+$app->post('/users', function (Request $request, Response $response) {
     $id = $request->getParam('id');
-    $sql = "SELECT * FROM users WHERE id= $id";
+    if( $id )$sql = "SELECT * FROM users WHERE id= $id";
+    else $sql = "SELECT * FROM users";
     try{
         $db = new db();
         $db = $db->connect();
@@ -31,26 +32,14 @@ $app->post('/hello', function (Request $request, Response $response) {
         $stmt = $db->query( $sql );
         $users = $stmt->fetchAll( PDO::FETCH_OBJ );
         $db = null;
-
-        echo json_encode( $users );
+        if( count($users) < 1 ) return  '[{"code": 404, "error": "No results" }]';
+        return json_encode( $users );
     }catch ( PDOException $e){
-        echo '{"error": {"text": '.$e->getMessage().'}';
+        $err = json_encode( "{asd:'asdasd'}" );
+
+        return '[{ "code": "'.$e->getCode().'", "error": "'.$e->getMessage().'" }]' ;
     }
 });
 
-$app->post('/all', function (Request $request, Response $response) {
-    $sql = "SELECT * FROM users";
-    try{
-        $db = new db();
-        $db = $db->connect();
 
-        $stmt = $db->query( $sql );
-        $users = $stmt->fetchAll( PDO::FETCH_OBJ );
-        $db = null;
-
-        echo json_encode( $users );
-    }catch ( PDOException $e){
-        echo '{"error": {"text": '.$e->getMessage().'}';
-    }
-});
 $app->run();
---
+++ src/functions/functions.php
@@ -0,0 +1,59 @@
+<?php
+namespace functions;
+
+class functions{
+    function json_error( $code, $message=null ) {
+
+        if ( is_array($code) ) $res = $code;
+        else $res = [ 'code' => $code, 'message' => $message ];
+        echo json_encode( $res );
+        exit;
+    }
+    function http_post ($url, $data, $json_decode = false, $debug = false)
+    {
+        if ( ! is_array( $data ) ) $data_url = $data;
+        else $data_url = http_build_query ($data);
+        $data_len = strlen ($data_url);
+        $context = stream_context_create (
+            array (
+                'http'=> array (
+                    'method'    => 'POST',
+                    'protocol_version' => 1.1,
+                    'header'    =>"Content-Type: application/x-www-form-urlencoded\r\nConnection: close\r\nContent-Length: $data_len\r\n",
+                    'content'   => $data_url,
+                    'timeout'   => 4
+                )
+            )
+        );
+        if ($debug) {
+            echo "request url: " . SERVER_URL . "?$data_url\n";
+        }
+        $content = file_get_contents ( $url, false, $context );
+        if ( $json_decode ) {
+            $re = @json_decode( $content, true );
+            if ( $error = json_decode_error() ) {
+                return ['code' => -11, 'message' => " >> Failed on decode JSON data from server $error - It may be server error. Data from server: $content"];
+            }
+            else return $re;
+        }
+        else return $content;
+    }
+
+    function json_decode_error() {
+        $error = json_last_error();
+        if ( $error ) {
+
+            switch ( $error ) {
+                case JSON_ERROR_NONE: return ' - No errors';
+                case JSON_ERROR_DEPTH: return' - Maximum stack depth exceeded';
+                case JSON_ERROR_STATE_MISMATCH: return ' - Underflow or the modes mismatch';
+                case JSON_ERROR_CTRL_CHAR: return ' - Unexpected control character found';
+                case JSON_ERROR_SYNTAX: return ' - Syntax error, malformed JSON';
+                case JSON_ERROR_UTF8: return ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                default: return ' - Unknown error';
+            }
+
+        }
+        else return false;
+    }
+}
